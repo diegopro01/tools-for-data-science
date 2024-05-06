022c1df961dd0dc91d434a30be6dccfb3842fb49
diegopro01 <diegosebastianpro2@gmail.com>
2024-05-05 20:08:20 -0500

    DataScienceEcosystem

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# Welcome to a comprehensive Python notebook designed to the questions of this assignment 
+
+# Some of the popular languages that Data Scientists use are:
+# - Python
+# - R
+# - SQL
+# 
+
+# Some of the commonly used NumPy libraries used by Data Scientists include:
+# - NumPy
+# - Pandas
+# - Matplotlib
+# - Seaborn
+# - Scikit-learn
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | RStudio            |
+# | VS Code            |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+# In Python, you can perform various arithmetic operations such as addition, subtraction, multiplication, and division using mathematical operators like `+`, `-`, `*`, and `/`.
+# 
+# Here are some examples:
+# 
+# - Addition: `5 + 5 = 10`
+# - Subtraction: `8 - 2 = 6`
+# - Multiplication: `1 * 3 = 3`
+# - Division: `8 / 4 = 2.0`
+# 
+# You can also use parentheses to control the order of operations, such as `(2 + 3) * 4 = 20`.
+# 
+
+# In[4]:
+
+
+x = 8 * 4 
+y = 10 + 5
+
+
+# In[5]:
+
+
+print(x)
+print(y)
+
+
+# In[7]:
+
+
+minutes = 600
+hours = minutes / 60
+
+print(f"{minutes} minutes is equal to {hours:.2f} hours")
+
+
+# ## List this Book Has Covered: 
+# - List popular languages for Data Science.
+# - List of most popular Python Libraries.
+# - Perform basic arithmetic operations in Python.
+# - Convert between minutes and hours.
+
+# ### Author Name
+# 
+# - Author: `Diego Proaño`
+# - Date: `May 5 of 2024`
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
